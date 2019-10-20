b0fd0c8048f825d565c4dba6f82e4cb2a424e6c9
BohdanRomaniuk <bohdan2307@gmail.com>
2019-10-20 13:12:53 +0300

    Create Initial projects

---
+++ 2) Weinstein and Fiker methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2__Weinstein_and_Fiker_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
