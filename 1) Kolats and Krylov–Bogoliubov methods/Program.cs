daa869d9c94ce944876f2af8dd41a005cd692b9d
BohdanRomaniuk <bohdan2307@gmail.com>
2019-10-20 16:41:39 +0300

    Finish Kolats Krylov Bogoliubov methods

--- 1) Kolats and Krylov–Bogoliubov methods/Program.cs
+++ 1) Kolats and Krylov–Bogoliubov methods/Program.cs
@@ -1,16 +1,141 @@
-﻿using System;
+﻿using MathNet.Numerics.LinearAlgebra.Double;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace _1_Kolats_and_KrylovBogoliubov_methods
 {
     class Program
     {
+        private const double left = 0;
+        private const double right = 1;
+        private const int size = 10; // size > 5
+        private const double h = (right - left) / (size - 1);
+        private const double l2 = 1.43589;
+
+        private static Matrix CreateMatrix()
+        {
+            var matrix = new DenseMatrix(size, size);
+            for (int i = 0; i < size; ++i)
+            {
+                matrix[i, i] = -2;
+                if (i + 1 < size)
+                {
+                    matrix[i, i + 1] = 1;
+                    matrix[i + 1, i] = 1;
+                }
+            }
+            return matrix;
+        }
+
+        private static Vector GetInitialF()
+        {
+            var vector = new DenseVector(size);
+            for (int i = 0; i < size; ++i)
+            {
+                vector[i] = 1;
+            }
+            return vector;
+        }
+
+        private static double Integrate(Vector u, Vector v)
+        {
+            double sum = 0;
+            for (int i = 0; i < size; ++i)
+            {
+                sum += u[i] * v[i] * h;
+            }
+            return sum;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Matrix T = CreateMatrix();
+            List<Vector> f = new List<Vector>();
+            f.Add(GetInitialF());
+
+            Console.WriteLine("T matrix:\t\t\t\t\t\t\t F vector:");
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    Console.Write($"{T[i, j],-5}{(j == size - 1 ? "\t\t\t" + f[0][i].ToString() : string.Empty)}");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //Iterations count
+            int itersCount = 15;
+
+            // Kolats method begin
+            ++itersCount;
+            for (int i = 0; i < itersCount - 1; ++i)
+            {
+                var fCurrent = f.Last(); //fi-1
+                var fNext = T.Solve(fCurrent) as Vector; //fi
+                f.Add(fNext);
+            }
+
+            // a0, a1, a2, ... (Shwartz constants)
+            double[] a = new double[itersCount];
+            for (int i = 0; i < itersCount; ++i)
+            {
+                a[i] = Integrate(f[0], f[i]);
+
+            }
+
+            // μ calculation
+            double[] mu = new double[itersCount];
+            for (int i = 1; i < itersCount; ++i)
+            {
+                mu[i] = a[i - 1] / a[i];
+            }
+
+            // ν calculation
+            double[] nu = new double[itersCount];
+            for (int i = 2; i < itersCount; ++i)
+            {
+                nu[i] = mu[i] - (mu[i] - mu[i - 1]) / (l2 / mu[i] - 1);
+            }
+
+            // λ calculation
+            double[] lambdaList = new double[itersCount];
+            for (int i = 2; i < itersCount; ++i)
+            {
+                lambdaList[i] = (mu[i] + nu[i]) / 2;
+            }
+
+            // Kolats method results
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Kolats method:");
+            Console.WriteLine("{0,3} {1,10} {2,15} {3,15} ", "Iter.", "ν", "λ", "μ");
+            for (int i = 2; i < itersCount; ++i)
+            {
+                Console.WriteLine("{0}:\t{1:F10}\t{2:F10}\t{3:F10}", i, nu[i], lambdaList[i], mu[i]);
+            }
+            Console.WriteLine();
+
+            // Krylov-Bogoliubov method begin
+            // μ calculation
+            double[] lowerBound = new double[itersCount];
+            double[] upperBound = new double[itersCount];
+            for (int i = 2; i < itersCount; ++i)
+            {
+                lowerBound[i] = mu[i] - Math.Sqrt(Math.Abs((mu[i - 1] - mu[i]) * mu[i]));
+                upperBound[i] = mu[i] + Math.Sqrt(Math.Abs((mu[i - 1] - mu[i]) * mu[i]));
+                lambdaList[i] = (upperBound[i] + lowerBound[i]) / 2;
+            }
+
+            // Krylov-Bogoliubov method results
+            Console.WriteLine("Krylov-Bogoliubov method:");
+            Console.WriteLine("i:\t\tμ lower\t\tλ\t\tμ upper");
+            for (int i = 2; i < itersCount; ++i)
+            {
+                Console.WriteLine("{0}:\t{1:F10}\t{2:F10}\t{3:F10}", i, lowerBound[i], lambdaList[i], upperBound[i]);
+            }
+
             Console.ReadKey();
         }
     }
--- 2) Weinstein and Fiker methods/Program.cs
+++ 2) Weinstein and Fiker methods/Program.cs
@@ -6,11 +6,222 @@ using System.Threading.Tasks;
 
 namespace _2__Weinstein_and_Fiker_methods
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
+        #region Fields
 
+        public double a { get; set; }
+        public double b { get; set; }
+        public int number { get; set; }
+        public double h { get { return (b - a) / (number - 1); } } // If we have 50 nodes, then we have 49 segments
+
+        public double[,] A { get; set; }
+        public double[,] B { get; set; }
+
+        #endregion
+
+        #region Some methods
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="a">Lower bound</param>
+        /// <param name="b">Upper bound</param>
+        /// <param name="n">A & B matrix size</param>
+        public Program(double a, double b, int n)
+        {
+            this.A = A;
+            this.B = B;
+            this.a = a;
+            this.b = b;
+            this.number = n;
+            SetAandB();
+        }
+
+        public void SetAandB()
+        {
+            // T - matrix with only 1/h^2 * [ ... 1 -2 1 ... ]
+            double[,] T = new double[number, number];
+
+            #region Only matrix
+            T[0, 0] = 1 / Math.Pow(h, 2);
+            for (int i = 1; i < number; ++i)
+            {
+                for (int j = 0; j < number - 1; ++j)
+                {
+                    if (j == i)
+                    {
+                        T[i, j] = -2 / Math.Pow(h, 2);
+                    }
+                    if (j == i - 1 || j == i + 1)
+                    {
+                        T[i, j] = 1 / Math.Pow(h, 2);
+                    }
+                }
+            }
+            T[number - 1, number - 1] = 1 / Math.Pow(h, 2);
+            #endregion
+
+            double[,] A = (double[,])T.Clone();
+            double[,] B = (double[,])T.Clone();
+
+            for (int i = 0; i < number; ++i)
+            {
+                double x = Math.Pow(4 - Math.Pow(a + i * h, 2), 2);
+                double y = 2 * Math.Pow(4 - Math.Pow(a + i * h, 2), 2);
+
+                for (int j = 0; j < number; ++j)
+                {
+                    A[i, j] *= -x;
+                    B[i, j] *= y;
+                }
+            }
+
+            this.A = A;
+            this.B = B;
+        }
+
+        /// <summary>
+        /// Return matrix C. C = A - B.
+        /// </summary>
+        /// <param name="aMatr">A matrix</param>
+        /// <param name="bMatr">B matrix</param>
+        /// <param name="n">A & B matrix size</param>
+        /// <returns></returns>
+        public double[,] GetC(double[,] aMatr, double[,] bMatr, int n)
+        {
+            double[,] cMatr = new double[n, n];
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    cMatr[i, j] = aMatr[i, j] - bMatr[i, j];
+                }
+            }
+            return cMatr;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="k">The power of function</param>
+        /// <returns></returns>
+        public double[] GetFk(int k)
+        {
+            double[] fk = new double[number];
+            for (int i = 0; i < number; ++i)
+            {
+                //fk[i] = Math.Pow(a + i * h, k); // x^k
+                fk[i] = Math.Sin(k * (a + i * h)); // sin(kx)
+            }
+            return fk;
+        }
+
+        public double[,] GetCn(List<double[]> g, int n)
+        {
+            double[,] cn = new double[n, n];
+            for (int i = 0; i < n; ++i)
+            {
+                double elem = 0;
+                for (int j = 0; j < n; ++j)
+                {
+                    double b = 0; // TODO: sum(g)/n
+                    for (int jj = 0; jj < n; ++jj)
+                    {
+                        b += g[i][jj];
+                    }
+                    b /= n;
+
+                    elem += b * g[i][j];
+                }
+            }
+            return cn;
+        }
+
+        public double[,] GetAn(double[,] B, double[,] Cn)
+        {
+            double[,] result = new double[number, number];
+
+            for (int i = 0; i < number; ++i)
+            {
+                for (int j = 0; j < number; ++j)
+                {
+                    result[i, j] = B[i, j] + Cn[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fk">fk</param>
+        /// <returns>gk</returns>
+        public double[] GetGk(double[,] cMatr, double[] fk)
+        {
+            // gk = C * fk
+            double[] gk = new double[number];
+            for (int i = 0; i < number; ++i)
+            {
+                gk[i] = 0;
+                for (int j = 0; j < number; ++j)
+                {
+                    gk[i] += cMatr[i, j] * fk[j];
+                }
+            }
+            return gk;
+        }
+
+        #endregion
+
+        public void Calculate()
+        {
+            double[,] C = GetC(A, B, number);
+
+            List<double[]> listFk = new List<double[]>();
+            for (int k = 0; k < 50; ++k)
+            {
+                listFk.Add(GetFk(k));
+            }
+
+            List<double[]> listGk = new List<double[]>();
+            for (int k = 0; k < 50; ++k)
+            {
+                listGk.Add(GetGk(C, listFk[k]));
+            }
+
+            // Find Cn, find An, using lib find ...values of An.
+            double[,] Cn = GetCn(listGk, number);
+            double[,] An = GetAn(B, Cn);
+
+            double[] d = new double[number];
+            double[] e = new double[number];
+            double[] result = new double[number];
+
+            for (int i = 0; i < number; ++i)
+            {
+                d[i] = An[i, i];
+                if (i == 0)
+                {
+                    e[i] = d[i + 1];
+                }
+                else
+                {
+                    e[i] = d[i - 1];
+                }
+            }
+
+            alglib.smatrixtdevd(ref d, e, number, 0, ref An);
+
+            Console.WriteLine(d.Length + " " + e.Length + " " + An.Length);
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    Console.WriteLine(d[i] + " " + e[i]);
+                }
+            }
         }
     }
 }
