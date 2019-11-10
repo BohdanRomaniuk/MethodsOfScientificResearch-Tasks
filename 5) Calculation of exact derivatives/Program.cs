24d48631ec5f410499483aa2acc32a3da6e6d4a5
BohdanRomaniuk <bohdan2307@gmail.com>
2019-11-10 19:42:02 +0200

    Simplify 5 and add 6

--- 5) Calculation of exact derivatives/Program.cs
+++ 5) Calculation of exact derivatives/Program.cs
@@ -53,63 +53,93 @@ namespace Calculation_of_exact_derivatives
             return GetMatrixT().Multiply(diagonalMatrix);
         }
 
-        private static double GetDeltaLambda(double[,] matrix, int n)
+        private static double[,] GetMatrixD(double[,] A, double value, int n)
         {
-            var b = new double[n, n];
-            var c = new double[n, n];
-            var d = matrix.Copy();
-            b.FillDiagonal(1);
+            var D = new double[n, n];
+            D = D.FillDiagonal(value);
+            return A.Sub(D);
+        }
 
-            var u = new double[n, n];
-            var l = new double[n, n];
+        public static void LU(double[,] D, out double[,] L, out double[,] U)
+        {
+            L = new double[n,n];
+            U = new double[n, n];
 
-            var v = new double[n, n];
-            var m = new double[n, n];
-            var w = new double[n, n];
-            var nn = new double[n, n];
-
-            for (int r = 0; r < n; ++r)
+            for (int i = 0; i < n; ++i)
             {
-                int k = r;
-                for (int i = r + 1; i < n; ++i, ++k)
+                U[0, i] = D[0, i];
+                for (int j = i; j < n; ++j)
                 {
-                    u[r, k] = d[r, k] - l.Row(r).Multiply(u.Col(k)).SumFirst(r);
-                    l[i, r] = (d[i, r] - l.Row(i).Multiply(u.Col(r)).SumFirst(r)) / u[r, r];
-
-                    v[r, k] = b[r, k] - m.Row(r).Multiply(u.Col(k)).Sum(l.Row(r).Multiply(v.Col(k))).SumFirst(r);
-                    m[i, r] = (b[i, r] - m.Row(i).Multiply(u.Col(r)).Sum(l.Row(i).Multiply(v.Col(r))).SumFirst(r) - l[i, r] * v[r, r]) / u[r, r];
-
-                    w[r, k] = c[r, k] - nn.Row(r).Multiply(u.Col(k)).Sum(m.Row(r).Multiply(v.Col(k), 2)).Sum(l.Row(r).Multiply(w.Col(k))).SumFirst(r);
-
-                    nn[i, r] = (c[i, r] - nn.Row(i).Multiply(u.Col(r)).Sum(m.Row(i).Multiply(v.Col(r), 2)).Sum(l.Row(i).Multiply(w.Col(r)))
-                        .SumFirst(r) - 2 * m[i, r] *v[r, r] - l[i, r] * w[r, r]) / u[r, r];
+                    var sum = 0.0;
+                    for (int k = 0; k < i; ++k)
+                    {
+                        sum += L[i, k] * U[k, j];
+                    }
+                    U[i, j] = D[i, j] - sum;
+                    if (i <= j)
+                    {
+                        sum = 0.0;
+                        for (int k = 0; k < i; ++k)
+                        {
+                            sum += L[j, k] * U[k, i];
+                        }
+                        L[j, i] = (D[j, i] - sum) / U[i, i];
+                    }
                 }
-                k = n - 1;
-
-                u[r, k] = d[r, k] - l.Row(r).Multiply(u.Col(k)).SumFirst(r);
-                v[r, k] = b[r, k] - m.Row(r).Multiply(u.Col(k)).Sum(l.Row(r).Multiply(v.Col(k))).SumFirst(r);
-                w[r, k] = c[r, k] - nn.Row(r).Multiply(u.Col(k)).Sum(m.Row(r).Multiply(v.Col(k), 2)).Sum(l.Row(r).Multiply(w.Col(k))).SumFirst(r);
             }
+        }
 
-            l.FillDiagonal(1);
-            m.FillDiagonal(0);
-            nn.FillDiagonal(0);
+        public static void MULV(double[,] B, out double[,] M, double[,] U, double[,] L, out double[,] V)
+        {
+            M = new double[n,n];
+            V = new double[n, n];
+
+            for (int i = 0; i < n; ++i)
+            {
+                V[0, i] = B[0, i];
+                for (int j = i; j < n; ++j)
+                {
+                    var sum = 0.0;
+                    for (int k = 0; k < i; ++k)
+                    {
+                        sum += M[i, k] * U[k, j] + L[i, k] * V[k, j];
+                    }
+                    V[i, j] = B[i, j] - sum;
+                    if (i <= j)
+                    {
+                        sum = 0;
+                        for (int k = 0; k < i; ++k)
+                        {
+                            sum += M[j, k] * U[k, i] + L[j, k] * V[k, i];
+                        }
+                        M[j, i] = (B[j, i] - sum - L[j, i] * V[i, i]) / U[i, i];
+                    }
+                }
+            }
+        }
+
+        private static double GetDeltaLambda(double[,] D, int n)
+        {
+            var B = new double[n, n];
+            var C = new double[n, n];
+            B.FillDiagonal(1);
+
+            var U = new double[n, n];
+            var L = new double[n, n];
+            var V = new double[n, n];
+            var M = new double[n, n];
+
+            LU(D, out L, out U);
+            MULV(B, out M, U, L, out V);
 
             double sum = 0;
             for(int i=0; i<n; ++i)
             {
-                sum += v[i, i] / u[i, i];
+                sum += V[i, i] / U[i, i];
             }
 
             //Delta lamdba
             return 1 / sum; 
-        }
-
-        private static double[,] SubDiagonal(double[,] matrix, double value, int n)
-        {
-            var sub = new double[n, n];
-            sub = sub.FillDiagonal(value);
-            return matrix.Sub(sub);
         }
 
         static void Main(string[] args)
@@ -118,22 +148,22 @@ namespace Calculation_of_exact_derivatives
             right = 1;
             n = 4;
             h = (right - left) / (n - 1);
-            var deviation = 0.00001;
+            var deviation = 0.00000001;
             var A = GetMatrixA();
 
-            double lprev = 33;
-            double lnext = 0;
+            double lambdaPrev = 33;
+            double lambdaNext = 0;
             double deltaLambda = double.MaxValue;
             do
             {
-                var matrix = SubDiagonal(A, lprev, n);
-                deltaLambda = GetDeltaLambda(matrix, n);
+                var D = GetMatrixD(A, lambdaPrev, n);
+                deltaLambda = GetDeltaLambda(D, n);
 
-                lnext = lprev + deltaLambda;
-                lprev = lnext;
+                lambdaNext = lambdaPrev + deltaLambda;
+                lambdaPrev = lambdaNext;
             } while (deltaLambda > deviation);
 
-            Console.WriteLine($"First = {lnext}");
+            Console.WriteLine($"First = {lambdaNext}");
             var decA = new Accord.Math.Decompositions.EigenvalueDecomposition(A, false, true);
             var eigValsA = decA.RealEigenvalues.OrderBy(c => c);
             Console.WriteLine($"Real  = { string.Join("\t", eigValsA.Select(c => c))}");
--- 6) Finding the initial approximation/Program.cs
+++ 6) Finding the initial approximation/Program.cs
@@ -3,39 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
+using MathNet.Numerics.LinearAlgebra.Complex;
 
 namespace Finding_the_initial_approximation
 {
     using Helpers;
+    using MathNet.Numerics.LinearAlgebra;
+
     class Program
     {
-        private static double left;
-        private static double right;
-        private static int n;
-        private static double h;
+        public const double LowerBound = 0.0 ;
+        public const double UpperBound = 1.0 ;
+        public const int N = 50;
+        public const double H = 1.0 / (N + 1);
+        public const double Center = 65.0;
+        public const double Rho = 2.0;
 
-        public static double F(double x)
+        private static double F(double x)
         {
             return (-1) * ((9 + x * x) * (9 + x * x));
         }
 
-        public static double[] GetDiscreteFunction()
+        private static Vector<Complex> Vector()
         {
-            var res = new double[n];
-            for (int i = 0; i < n; ++i)
+            Vector<Complex> vector = Vector<Complex>.Build.Dense(N);
+            for (int i = 0; i < N; ++i)
             {
-                res[i] = F(left + i * h);
+                vector[i] = F(LowerBound + i * H);
             }
-            return res;
+            return vector;
         }
 
-        private static double[,] GetMatrixT()
+        private static Matrix<Complex> GetMatrixA()
         {
-            var matrix = new double[n, n];
-            for (int i = 0; i < n; ++i)
+            return Matrix<Complex>.Build.DiagonalOfDiagonalVector(Vector());
+        }
+
+        private static Matrix<Complex> GetMatrixT()
+        {
+            var matrix = Matrix<Complex>.Build.Sparse(N, N);
+            for (int i = 0; i < N; ++i)
             {
                 matrix[i, i] = -2;
-                if (i + 1 < n)
+                if (i + 1 < N)
                 {
                     matrix[i, i + 1] = 1;
                     matrix[i + 1, i] = 1;
@@ -44,19 +55,177 @@ namespace Finding_the_initial_approximation
             return matrix;
         }
 
-        private static double[,] GetMatrixA()
+        public static Matrix<Complex> LinearOperator(Complex lambda)
         {
-            var f = GetDiscreteFunction();
-            var diagonalMatrix = new double[n, n];
-            for (int i = 0; i < n; ++i)
+            return GetMatrixT() - lambda * H2() * GetMatrixA();
+        }
+
+        public static Matrix<Complex> LinearOperatorDerivative()
+        {
+            return -H2() * GetMatrixA();
+        }
+
+        private static double H2()
+        {
+            return Math.Pow(H, 2);
+        }
+
+        public static void LU(Matrix<Complex> D, out Matrix<Complex> L, out Matrix<Complex> U)
+        {
+            L = Matrix<Complex>.Build.Sparse(N, N);
+            U = Matrix<Complex>.Build.Sparse(N, N);
+
+            for (int i = 0; i < N; ++i)
             {
-                diagonalMatrix[i, i] = f[i];
+                U[0, i] = D[0, i];
+
+                for (int j = i; j < N; ++j)
+                {
+                    Complex summation = Complex.Zero;
+
+                    for (int k = 0; k < i; ++k)
+                    {
+                        summation += L[i, k] * U[k, j];
+                    }
+
+                    U[i, j] = D[i, j] - summation;
+
+                    if (i <= j)
+                    {
+                        summation = Complex.Zero;
+
+                        for (int k = 0; k < i; ++k)
+                        {
+                            summation += L[j, k] * U[k, i];
+                        }
+
+                        L[j, i] = (D[j, i] - summation) / U[i, i];
+                    }
+                }
             }
-            return GetMatrixT().Multiply(diagonalMatrix);
+        }
+
+        public static void MULV(Matrix<Complex> B, out Matrix<Complex> M, Matrix<Complex> U, Matrix<Complex> L, out Matrix<Complex> V)
+        {
+            M = Matrix<Complex>.Build.Sparse(N, N);
+            V = Matrix<Complex>.Build.Sparse(N, N);
+
+            for (int i = 0; i < N; ++i)
+            {
+                V[0, i] = B[0, i];
+
+                for (int j = i; j < N; ++j)
+                {
+                    Complex summation = Complex.Zero;
+
+                    for (int k = 0; k < i; ++k)
+                    {
+                        summation += M[i, k] * U[k, j] + L[i, k] * V[k, j];
+                    }
+
+                    V[i, j] = B[i, j] - summation;
+
+                    if (i <= j)
+                    {
+                        summation = Complex.Zero;
+
+                        for (int k = 0; k < i; ++k)
+                        {
+                            summation += M[j, k] * U[k, i] + L[j, k] * V[k, i];
+                        }
+
+                        M[j, i] = (B[j, i] - summation - L[j, i] * V[i, i]) / U[i, i];
+                    }
+                }
+            }
+        }
+
+        public static Complex Determinant(Matrix<Complex> U)
+        {
+            Complex result = Complex.One;
+            for (int i = 0; i < U.RowCount; ++i)
+            {
+                result *= U[i, i];
+            }
+            return result;
+        }
+
+        public static Complex DeterminantDerivative(Matrix<Complex> V, Matrix<Complex> U)
+        {
+            var sum = Complex.Zero;
+            for (int k = 0; k < V.RowCount; ++k)
+            {
+                Complex product = Complex.One;
+                for (int i = 0; i < U.RowCount; ++i)
+                {
+                    if (i != k)
+                    {
+                        product *= U[i, i];
+                    }
+                }
+                sum += V[k, k] * product;
+            }
+
+            return sum;
+        }
+
+        public static double RootsCount()
+        {
+            Complex summation = Complex.Zero;
+
+            for (int i = 0; i < N; ++i)
+            {
+                var spectralRadius = SpectralRadius(i + 1, N);
+                var lambda = Center + spectralRadius;
+
+                Matrix<Complex> D = LinearOperator(lambda);
+                Matrix<Complex> B = LinearOperatorDerivative();
+                Matrix<Complex> L, U, M, V;
+
+                LU(D, out L, out U);
+                MULV(B, out M, U, L, out V);
+
+                var determinant = Determinant(U);
+                var determinantDerivative = DeterminantDerivative(V, U);
+
+                summation += spectralRadius * determinantDerivative / determinant;
+            }
+
+            return (summation / N).Magnitude;
+        }
+
+        public static double InitialApproximation(int j, int n)
+        {
+            return (Center + SpectralRadius(j, n)).Magnitude;
+        }
+
+        private static Complex SpectralRadius(int j, int n)
+        {
+            return Rho * Complex.Exp(Complex.ImaginaryOne * 2.0 * Math.PI * j / n);
         }
 
         static void Main(string[] args)
         {
+            double s0 = RootsCount();
+            Console.WriteLine("s0 = {0}\n", s0);
+
+            int rootsCount = (int)Math.Round(s0);
+            if (rootsCount > 0)
+            {
+                Console.WriteLine("{0} root(s) found. Initial Approximation(s):\n", rootsCount);
+                for (int i = 0; i < rootsCount; ++i)
+                {
+                    double initalApproximation = InitialApproximation(i + 1, rootsCount);
+
+                    Console.WriteLine("{0}. lambda0 = {1}", i + 1, initalApproximation / 2);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No roots found.");
+            }
+
+            Console.ReadKey();
         }
     }
 }
