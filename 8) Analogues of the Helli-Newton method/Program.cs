5da333bf7feffe0a7a0bf981376c6ed2ff81178e
BohdanRomaniuk <bohdan2307@gmail.com>
2019-11-13 23:20:44 +0200

    Started work on 7 lab

---
+++ 8) Analogues of the Helli-Newton method/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8__Analogues_of_the_Helli_Newton_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
